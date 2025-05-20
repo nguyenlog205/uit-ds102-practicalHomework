a598bd1727b96751e95f2421f8f97eeac342a434
Long Nguyen Hoang <longnguyenhoang.personal@gmail.com>
2025-05-20 20:47:02 +0700

    update lesson 05

--- lesson-05/src/word_segmentation.py
+++ lesson-05/src/word_segmentation.py
@@ -12,7 +12,7 @@ def load_dataset() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame] | None:
     This function loads dataset for further analysis. It returns train, dev, test respectively.
     '''
 
-    logger.info('Start loading dataset ...')
+    logger.info('Start loading datasets ...')
     
     # train dataset 
     try:  
@@ -22,6 +22,7 @@ def load_dataset() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame] | None:
         logger.error(f'Error loading train dataset: {e}')
         return None
     
+    # dev dataset 
     try:
         df_dev = pd.read_json('./lesson-05/data/UIT-VSFC-dev.json', lines=False)
         print(df_dev.head())
@@ -29,7 +30,7 @@ def load_dataset() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame] | None:
         logger.error(f'Error loading dev dataset: {e}')
         return None
     
-
+    # test dataset 
     try:
         df_test = pd.read_json('./lesson-05/data/UIT-VSFC-test.json', lines=False)
         print(df_test.head())
@@ -37,15 +38,29 @@ def load_dataset() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame] | None:
         logger.error(f'Error loading test dataset: {e}')
         return None
     
-    logger.success('Loading dataset has been completed successfully!')
+    logger.success('Loading datasets has been completed successfully!')
 
     return df_train, df_dev, df_test
+
+def tokenize_string(string: str) -> list[str] | None:
+
+    return None
+
+def tokenize_stringInDatasets(datasets: tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    '''
+    This function is to return the tokenized datasets.
+    Respectively train, dev, test datasets.
+    '''
+    tokenized_df_train = {}
+    tokenized_df_dev   = {}
+    tokenized_df_test  = {}
+
 
 
 def main():
     datasets = load_dataset()
-    if datasets is not None:
-        df_train, df_dev, df_test = datasets
+    if datasets is None:
+        logger.error('The pipeline has been interrupted just now!')
 
 if __name__ == '__main__':
     main()
